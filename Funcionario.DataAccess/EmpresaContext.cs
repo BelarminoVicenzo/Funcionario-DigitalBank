2aee15ad414a1fb8a2128ddfdacceb4421a6bfb9
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-02-19 10:09:17 +0100

    created funcionario.dataaccess project

---
+++ Funcionario.DataAccess/EmpresaContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Funcionario.Models;
+
+namespace Funcionario.DataAccess
+{
+    public class EmpresaContext : DbContext
+    {
+
+
+        public EmpresaContext() : base("name=Empresa")
+        {
+            //prevent creating a new db
+            Database.SetInitializer<EmpresaContext>(null);
+        }
+
+        public DbSet<Models.Funcionario> Funcionario { get; set; }
+        public DbSet<Estado> Estado { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // EF is search for plural names, this will prevent
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+ 
+
+    }
+
+}
