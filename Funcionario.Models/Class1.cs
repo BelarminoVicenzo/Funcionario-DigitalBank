6977119383b37a7d070330b0070498acf7dfcd09
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-02-19 11:54:42 +0100

    added error messages for required fields

--- Funcionario.Models/Class1.cs
+++ Funcionario.Models/Class1.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-    using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace Funcionario.Models
 {
 
     public partial class Estado
     {
-        
+
         public Estado()
         {
             //avoid NullReferenceException
@@ -22,7 +22,7 @@ namespace Funcionario.Models
         public string Sigla { get; set; }
         public string Nome { get; set; }
 
-            public virtual ICollection<Funcionario> Funcionario { get; set; }
+        public virtual ICollection<Funcionario> Funcionario { get; set; }
     }
 
 
@@ -31,13 +31,14 @@ namespace Funcionario.Models
 
         [Key]
         public int PK_Funcionario { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Este campo não pode estar vazio")]
         public string Nome { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "Este campo não pode estar vazio")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Este campo não pode estar vazio")]
         [DataType(DataType.Date)]
         public System.DateTime DataNascimento { get; set; }
         public Nullable<decimal> Salario { get; set; }
