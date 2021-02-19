a8b9d4a626cc36451e6cdbc03b34c756eb44adef
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-02-19 10:08:53 +0100

    created funcionario.models project

---
+++ Funcionario.Models/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
+
+namespace Funcionario.Models
+{
+
+    public partial class Estado
+    {
+        
+        public Estado()
+        {
+            //avoid NullReferenceException
+            this.Funcionario = new List<Funcionario>();
+        }
+
+        [Key]
+        public int PK_Estado { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+
+            public virtual ICollection<Funcionario> Funcionario { get; set; }
+    }
+
+
+    public partial class Funcionario
+    {
+
+        [Key]
+        public int PK_Funcionario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public System.DateTime DataNascimento { get; set; }
+        public Nullable<decimal> Salario { get; set; }
+        public Nullable<int> FK_Estado { get; set; }
+
+        public virtual Estado Estado { get; set; }
+    }
+
+}
